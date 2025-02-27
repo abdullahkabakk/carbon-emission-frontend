e92843386ae707022b8ba601d2806ceed4d51aff
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2025-02-27 14:36:51 +0300

    feat: Add maplibregl and map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, shallowRef } from "vue";
+import maplibregl, {type MapOptions} from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const map = shallowRef<maplibregl.Map | null>(null);
+
+const LIGHTMAPTILEURL = "https://tiles.openfreemap.org/styles/bright";
+
+const config = shallowRef<MapOptions>({
+  container: "map",
+  style: LIGHTMAPTILEURL,
+  center: [13.388, 52.517],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+onMounted(() => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const {latitude, longitude} = position.coords;
+      config.value.center = [longitude, latitude];
+
+      map.value = new maplibregl.Map(config.value);
+
+      map.value.on("load", () => {
+        map.value?.addControl(new maplibregl.NavigationControl(), "bottom-left");
+      });
+    },
+    (error) => {
+      console.error("Error getting user location:", error);
+
+      map.value = new maplibregl.Map(config.value);
+    }
+  );
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col h-screen">
+    <!-- Main content area -->
+    <div class="relative flex flex-1">
+      <!-- Map container -->
+      <div id="map" class="h-full w-full"/>
+    </div>
+  </div>
 </template>
